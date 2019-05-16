a3df40bbbae1634ec6f3d675ee34c413f95bebda
David Lai <me@dlai0001.com>
2019-05-15 22:48:10 -0700

    add graphql yoga

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const os = require('os');
+const { GraphQLServer } = require('graphql-yoga');
 
-const app = express();
 
-app.use(express.static('dist'));
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`
 
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+const resolvers = {
+    Query: {
+        hello: (_, { name }) => `Hello ${name || 'World'}`,
+    },
+}
 
-app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT || 3000}!`));
+
+const graphqlserver = new GraphQLServer({ typeDefs, resolvers });
+
+graphqlserver.express.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+graphqlserver.express.use(express.static('dist'));
+
+// app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+graphqlserver.start(
+    {
+        port: 8080,
+        endpoint: '/graphql',
+        subscriptions: '/subscriptions',
+        playground: '/playground',
+    },
+    ({ port }) => console.log(`Server is running on localhost:${port}`)
+);
