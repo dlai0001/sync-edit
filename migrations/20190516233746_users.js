2cd31b5f1a0abdadaa9e399535b835e90dc00be4
David Lai <me@dlai0001.com>
2019-05-17 21:37:23 -0700

    add post install step:

--- migrations/20190516233746_users.js
+++ migrations/20190516233746_users.js
@@ -3,6 +3,7 @@ exports.up = function (knex, Promise) {
     return knex.schema
         .createTable('users', function (table) {
             table.increments('id');
+            table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
             table.string('name', 255).notNullable();
             table.string('phone_number', 255).notNullable();
             table.string('pin', 255).notNullable();
