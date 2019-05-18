b811617272668c4cb40d48aaa6c146487bc75fd5
David Lai <me@dlai0001.com>
2019-05-18 15:45:37 -0700

    fix

--- src/client/App.test.js
+++ src/client/App.test.js
@@ -6,14 +6,13 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
 function setup() {
-    const wrapper = shallow(<BrowserRouter><App/></BrowserRouter>);
+    const wrapper = shallow(<App/>);
     return { wrapper };
 }
 
 describe('App Test Suite', () => {
     it('should render', () => {
         const { wrapper } = setup();
-        debugger;
-        expect(wrapper.find('Router').exists()).toBe(true);
+        expect(wrapper.find('NavBar').exists()).toBe(true);
     });
 });
