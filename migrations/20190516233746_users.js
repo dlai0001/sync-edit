f04c2bcdba628ebcf6f1038208b7c82a70870bd2
David Lai <me@dlai0001.com>
2019-05-17 22:03:21 -0700

    update schema

--- migrations/20190516233746_users.js
+++ migrations/20190516233746_users.js
@@ -1,15 +1,20 @@
 
-exports.up = function (knex, Promise) {
+exports.up = function (knex) {
     return knex.schema
         .createTable('users', function (table) {
-            table.increments('id');
-            table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+            // This will store a uuid to be used a key.
+            table.string('id');            
+
+            // data fields
             table.string('name', 255).notNullable();
             table.string('phone_number', 255).notNullable();
             table.string('pin', 255).notNullable();
+
+            // Used to track latest version.
+            table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
         });
 };
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex) {
     return knex.schema.dropTable('users');
 };
--- src/server/db.js
+++ src/server/db.js
@@ -1,7 +1,6 @@
 const knexConfig = require('../../knexfile');
 
 const config = knexConfig[process.env.NODE_ENV] || knexConfig.development;
-console.log('Using db config', config);
 
 const knex = require('knex')(config);
 
