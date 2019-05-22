e053d97e652625011fb37679f4976a507d44d30c
David Lai <me@dlai0001.com>
2019-05-21 23:22:20 -0700

    change App test to look for switch instead of NavBar, since it's a shallow render

--- src/client/App.test.js
+++ src/client/App.test.js
@@ -3,16 +3,15 @@ import { shallow } from 'enzyme';
 
 // Components
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
 
 function setup() {
     const wrapper = shallow(<App/>);
     return { wrapper };
 }
 
-describe.skip('App Test Suite', () => {
+describe('App Test Suite', () => {
     it('should render', () => {
         const { wrapper } = setup();
-        expect(wrapper.find('NavBar').exists()).toBe(true);
+        expect(wrapper.find('Switch').exists()).toBe(true);
     });
 });
